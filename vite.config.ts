9ddcdc8ab169ce61407ef00eb3c3a6587d7063ba
illufoxKusanagi <ariefsatria2409@gmail.com>
2026-09-08 02:28:57 +0700

    fix: updating url

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import path from "node:path";
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import vue from "@vitejs/plugin-vue";
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   server: {
-    host: true, // Needed for Docker to expose outside the container
+    host: true, // Needed for Docker and cloud preview to expose outside the container
+    port: 5173,
+  },
+  preview: {
+    host: true,
     port: 5173,
   },
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
